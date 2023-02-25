7e27940719bb7e0d7d25c36727ea27aff2c958c8
Valeryia Tselesh <valeryiatselesh@gmail.com>
2023-02-25 16:54:55 +0100

    Add preloader. Image optimization. Bug fixing

--- js/script.js
+++ js/script.js
@@ -1,66 +1,77 @@
 "use strict";
 
-//Type of device
+typeOfDevice();
+accordionFaq();
+signUpForm();
 
-const isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	ios: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (
-			isMobile.Android() ||
-			isMobile.BlackBerry() ||
-			isMobile.ios() ||
-			isMobile.Opera() ||
-			isMobile.Windows());
-	}
+// Preloader
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 100);
 };
-if (isMobile.any()) {
-	document.body.classList.add('_touch');
 
-	let menuArrows = document.querySelectorAll('.menu__arrow');
-	if (menuArrows.length > 0) {
-		for (let index = 0; index < menuArrows.length; index++) {
-			const menuArrow = menuArrows[index];
-			menuArrow.addEventListener('click', function (e) {
-				menuArrow.parentElement.classList.toggle('_active');
-			});
+//Type of device
+function typeOfDevice() {
+	const isMobile = {
+		Android: function () {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function () {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		ios: function () {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function () {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function () {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function () {
+			return (
+				isMobile.Android() ||
+				isMobile.BlackBerry() ||
+				isMobile.ios() ||
+				isMobile.Opera() ||
+				isMobile.Windows());
 		}
+	};
+	if (isMobile.any()) {
+		document.body.classList.add('_touch');
+	
+		let menuArrows = document.querySelectorAll('.menu__arrow');
+		if (menuArrows.length > 0) {
+			for (let index = 0; index < menuArrows.length; index++) {
+				const menuArrow = menuArrows[index];
+				menuArrow.addEventListener('click', function (e) {
+					menuArrow.parentElement.classList.toggle('_active');
+				});
+			}
+		}
+	} else {
+		document.body.classList.add('_pc');
 	}
-} else {
-	document.body.classList.add('_pc');
 }
-
 
 //Burger Menu
+	const iconMenu = document.querySelector('.menu__icon');
+	const menuBody = document.querySelector('.menu__body');
+	const buttonMini = document.querySelector('.header__button_mini');
+	
+	if (iconMenu) {
+		iconMenu.addEventListener("click", function (e) {
+			document.body.classList.toggle('_lock');
+			iconMenu.classList.toggle('_active');
+			menuBody.classList.toggle('_active');
+			buttonMini.classList.toggle('_active');
+		});
+	}
 
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-const buttonMini = document.querySelector('.header__button_mini');
-if (iconMenu) {
-	iconMenu.addEventListener("click", function (e) {
-		document.body.classList.toggle('_lock');
-		iconMenu.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-		buttonMini.classList.toggle('_active');
-	});
-}
-
-
-// Slider swiper
-
+// Slider
 const swiper = new Swiper('.directions-slider', {
 	navigation: {
 		nextEl: '.swiper-button-next',
@@ -93,8 +104,6 @@ const swiper = new Swiper('.directions-slider', {
 });
 
 
-//Scroll block
-
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
 	menuLinks.forEach(menuLink => {
@@ -121,9 +130,7 @@ if (menuLinks.length > 0) {
 	}
 }
 
-
 // Scroll animation
-
 function onEntry(entry) {
 	entry.forEach(change => {
 	  if (change.isIntersecting) {
@@ -137,79 +144,74 @@ function onEntry(entry) {
  for (let elm of elements) {
 	observer.observe(elm);
 }
-  
 
-// Accordion faq
+// FAQ
+function accordionFaq() {
+	const items = document.querySelectorAll(".accordion button");
 
-const items = document.querySelectorAll(".accordion button");
+	function toggleAccordion() {
+		const itemToggle = this.getAttribute('aria-expanded');
+	
+		for (let i = 0; i < items.length; i++) {
+			items[i].setAttribute('aria-expanded', 'false');
+		}
+			
+		if (itemToggle == 'false') {
+			this.setAttribute('aria-expanded', 'true');
+			}
+		}
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (let i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
+		items.forEach(item => item.addEventListener('click', toggleAccordion));
 }
 
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+// Form
+function signUpForm() {
+	const select = document.querySelector('.select');
+	const selectValue = document.querySelector('.select_value');
+	const selectDropdown = document.querySelector('.select_dropdown');
 
+	selectDropdown.addEventListener('click', e => {
+		const option = e.target.closest('.select_option');
+		if (option) {
+			selectValue.textContent = option.textContent;
+			e.target.closest('.select').blur();
+			select.dispatchEvent(new CustomEvent('change', { detail: option.textContent }));
+		}
+	});
 
-// Sign up dropdown
-
-const select = document.querySelector('.select');
-const selectValue = document.querySelector('.select_value');
-const selectDropdown = document.querySelector('.select_dropdown');
-
-selectDropdown.addEventListener('click', e => {
-	const option = e.target.closest('.select_option');
-	if (option) {
-		selectValue.textContent = option.textContent;
-		e.target.closest('.select').blur();
-		select.dispatchEvent(new CustomEvent('change', { detail: option.textContent }));
-	}
-});
-
-
-// Sign up phone mask
-
-var phoneInput = document.querySelector(".user-box__phone");
-phoneInput.addEventListener("keydown", function (event) {
-  if (
-    !(
-      event.key == "ArrowLeft" ||
-      event.key == "ArrowRight" ||
-      event.key == "Backspace" ||
-      event.key == "Tab"
-    )
-  ) {
-    event.preventDefault();
-  }
-  var mask = "+(111) 111-111-111";
-
-  if (/[0-9\+\ \-\(\)]/.test(event.key)) {
-    var currentString = this.value;
-    var currentLength = currentString.length;
-    if (/[0-9]/.test(event.key)) {
-      if (mask[currentLength] == "1") {
-        this.value = currentString + event.key;
-      } else {
-        for (var i = currentLength; i < mask.length; i++) {
-          if (mask[i] == "1") {
-            this.value = currentString + event.key;
-            break;
-          }
-          currentString += mask[i];
-        }
-      }
-    }
-  }
-});
-
-
+	var phoneInput = document.querySelector(".user-box__phone");
+	phoneInput.addEventListener("keydown", function (event) {
+		if (
+			!(
+				event.key == "ArrowLeft" ||
+				event.key == "ArrowRight" ||
+				event.key == "Backspace" ||
+				event.key == "Tab"
+			)
+		) {
+			event.preventDefault();
+		}
+		
+	var mask = "+(111) 111-111-111";
+		if (/[0-9\+\ \-\(\)]/.test(event.key)) {
+			var currentString = this.value;
+			var currentLength = currentString.length;
+			if (/[0-9]/.test(event.key)) {
+				if (mask[currentLength] == "1") {
+					this.value = currentString + event.key;
+				} else {
+					for (var i = currentLength; i < mask.length; i++) {
+						if (mask[i] == "1") {
+							this.value = currentString + event.key;
+							break;
+						}
+						currentString += mask[i];
+					}
+				}
+			}
+		}
+	});
+}
 
 
 
